fb381697af1eb02b1bdf781c0c17470f05270638
jbrown34 <jbrown34@chicagobooth.edu>
2020-10-17 20:20:55 +0000

    base layer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  def addition
+    render({ :template => "math_views/addition_input.html.erb"})
+  end
+
+  def subtraction
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ # get("/lottery/unlucky", { :controller => "numbers", :action => "losers" })
+
+  get("/", {:controller => "application", :action => "addition"})
+
+  get("/add", {:controller => "application", :action => "addition"})
+
+  get("/subtract", {:controller => "application", :action => "subtraction"})
+
+  get("/multiply", {:controller => "application", :action => "multiplication"})
+
+  get("/divide", {:controller => "application", :action => "division"})
+
 end
